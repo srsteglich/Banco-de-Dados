51222818a6676c6f00c5558eeba2670ddf120ff0
Sérgio Renato Steglich <79202041+srsteglich@users.noreply.github.com>
2026-06-01 13:40:25 -0300

    Uncomment SQL queries for execution

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -4,45 +4,45 @@
 --SELECT * FROM FilmesGenero
 --SELECT * FROM Generos
 
--- Questão 1
--- SELECT Nome,Ano FROM Filmes
+-- QuestÃ£o 1
+SELECT Nome,Ano FROM Filmes
 
--- Questão 2
--- SELECT Nome,Ano,Duracao FROM Filmes ORDER BY Ano ASC 
+-- QuestÃ£o 2
+SELECT Nome,Ano,Duracao FROM Filmes ORDER BY Ano ASC 
 
--- Questão 3
--- SELECT Nome,Ano,Duracao FROM Filmes WHERE Nome LIKE 'de v%'
+-- QuestÃ£o 3
+SELECT Nome,Ano,Duracao FROM Filmes WHERE Nome LIKE 'de v%'
 
--- Questão 4
--- SELECT Nome,Ano,Duracao FROM Filmes WHERE Ano = '1997'
+-- QuestÃ£o 4
+SELECT Nome,Ano,Duracao FROM Filmes WHERE Ano = '1997'
 
--- Questão 5
--- SELECT Nome,Ano,Duracao FROM Filmes WHERE Ano > '2000'
+-- QuestÃ£o 5
+SELECT Nome,Ano,Duracao FROM Filmes WHERE Ano > '2000'
 
--- Questão 6
--- SELECT Nome,Ano,Duracao FROM Filmes WHERE Duracao > '100' AND Duracao < '150' ORDER BY Duracao ASC
+-- QuestÃ£o 6
+SELECT Nome,Ano,Duracao FROM Filmes WHERE Duracao > '100' AND Duracao < '150' ORDER BY Duracao ASC
 
--- Questão 7
--- SELECT Ano, COUNT(Ano) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC  
+-- QuestÃ£o 7
+SELECT Ano, COUNT(Ano) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC  
 
--- Questão 8
--- SELECT Id, PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'M'
+-- QuestÃ£o 8
+SELECT Id, PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'M'
 
--- Questão 9
--- SELECT Id, PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome ASC
+-- QuestÃ£o 9
+SELECT Id, PrimeiroNome, UltimoNome, Genero FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome ASC
 
--- Questão 10
--- SELECT F.Nome, G.Genero FROM Filmes F
---	 INNER JOIN FilmesGenero FG ON FG.IdFilme = F.Id
---	 INNER JOIN Generos G ON G.Id = FG.IdGenero
+-- QuestÃ£o 10
+SELECT F.Nome, G.Genero FROM Filmes F
+	 INNER JOIN FilmesGenero FG ON FG.IdFilme = F.Id
+	 INNER JOIN Generos G ON G.Id = FG.IdGenero
 
--- Questão 11
--- SELECT F.Nome, G.Genero FROM Filmes F
---	 INNER JOIN FilmesGenero FG ON FG.IdFilme = F.Id
---	 INNER JOIN Generos G ON G.Id = FG.IdGenero
---	 WHERE G.Genero LIKE 'Mi%'
+-- QuestÃ£o 11
+SELECT F.Nome, G.Genero FROM Filmes F
+	 INNER JOIN FilmesGenero FG ON FG.IdFilme = F.Id
+	 INNER JOIN Generos G ON G.Id = FG.IdGenero
+	 WHERE G.Genero LIKE 'Mi%'
 
---Questão 12
+--QuestÃ£o 12
 SELECT F.Nome, A.PrimeiroNome, A.UltimoNome, EF.Papel FROM ElencoFilme EF
   INNER JOIN Atores A ON A.Id = EF.IdAtor  
   INNER JOIN Filmes F ON F.Id = EF.IdFilme 
